4e2e2a686124040750ecb583f81446295e3eef94
신원식 <action_123@naver.com>
2024-02-20 16:17:01 +0900

    웹 완성

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,14 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('map.html')
 
-@app.route('/get_data', methods=('GET', 'POST'))
-def get_data():
-    data_for_gangnam = {
-        'images': ['동대문엽기떡볶이 강남점 긍정부정.jpg', '동대문엽기떡볶이 강남점 워드클라우드.jpg'],
-        'descriptions': ['강남점 이미지 1에 대한 설명', '강남점 이미지 2에 대한 설명']
-    }
-    print('겟데이타아아아ㅏ')
-    return jsonify(data_for_gangnam)
 
 if __name__ == '__main__':
     app.run(debug=True)
